cbc4158d99b1ee910f3dd94f0abe778269273ac4
Gary Marx <gary@zenpayroll.com>
2016-02-24 20:27:42 -0800

    complete assignment, censor title

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |post, i|
+      post.title = 'SPAM' if i % 5 == 0
+    end
   end
 
   def show
