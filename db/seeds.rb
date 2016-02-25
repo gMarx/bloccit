a4591ae8307b4fece551140966b659f17f3d404c
Gary Marx <gary@zenpayroll.com>
2016-02-24 16:21:36 -0800

    create unique post and related comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,16 @@ posts = Post.all
   )
 end
 
+Post.find_or_create_by(
+  title: 'Hakuna matata',
+  body: 'a Swahili phrase; translated, it roughly means "No worries". It is formed by the words hakuna (there is not here) and matata (plural form of problem).'
+)
+
+Comment.find_or_create_by(
+  post: Post.find_by(title: 'Hakuna matata', body: 'a Swahili phrase; translated, it roughly means "No worries". It is formed by the words hakuna (there is not here) and matata (plural form of problem).'),
+  body: 'That is where we are, ladies and gentlemen.'
+)
+
 puts 'Seed finished'
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
