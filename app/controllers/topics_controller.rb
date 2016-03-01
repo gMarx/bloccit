24e5290f2370b755df98e28f1d79eb6ab111cee5
Gary Marx <gary@zenpayroll.com>
2016-02-29 22:15:02 -0800

    update implemented

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -28,4 +28,19 @@ class TopicsController < ApplicationController
   def edit
     @topic = Topic.find(params[:id])
   end
+
+  def update
+    @topic = Topic.find(params[:id])
+    @topic.name = params[:topic][:name]
+    @topic.description = params[:topic][:description]
+    @topic.public = params[:topic][:public]
+
+    if @topic.save
+      flash[:notice] = 'Topic was updated successfully.'
+      redirect_to @topic
+    else
+      flash.now[:alert] = 'Error updating topic. Please try again.'
+      render :edit
+    end
+  end
 end
