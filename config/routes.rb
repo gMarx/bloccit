8a2eafcbdd23048ff289e45db9e0e3b379973e63
Gary Marx <gary@zenpayroll.com>
2016-02-29 21:31:19 -0800

    show & new Topic

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   resources :questions
   resources :advertisements
   resources :posts
+  resources :topics
 
   root 'welcome#index'
   get 'about' => 'welcome#about'
   get 'contact' => 'welcome#contact'
   get 'faq' => 'welcome#faq'
 
-  end
+end
