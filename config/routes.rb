3ac4a26064c2918a7a29cb9c87a21989e9ff675a
Gary Marx <gary@zenpayroll.com>
2016-02-29 22:53:12 -0800

    nest posts under topic

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   resources :questions
   resources :advertisements
-  resources :posts
-  resources :topics
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   root 'welcome#index'
   get 'about' => 'welcome#about'
