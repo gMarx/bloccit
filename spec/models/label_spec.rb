565c35919f7ebd8a6e57f916735198355ac2896a
Gary Marx <gary@zenpayroll.com>
2016-03-07 23:33:14 -0800

    labeling specs pass, added to Topics and Posts

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -1,8 +1,25 @@
 require 'rails_helper'
+require 'random_data'
 
 RSpec.describe Label, type: :model do
-  it { is_expected.to have_many :labelings }
+  let(:topic) { Topic.create!(name: RandomData.random_word, description: RandomData.random_paragraph, ) }
+  let(:user) { User.create!(name: 'Bloccit User', email: 'user@bloccit.com', password: 'helloworld') }
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence , body: RandomData.random_paragraph, user: user) }
+  let(:label) { Label.create!(name: 'Label') }
 
+  it { is_expected.to have_many :labelings }
   it { is_expected.to have_many(:topics).through(:labelings) }
   it { is_expected.to have_many(:posts).through(:labelings) }
+
+  describe 'labelings' do
+    it 'allows the same label to be associated with a differet topic and post' do
+      topic.labels << label
+      post.labels << label
+
+      topic_label = topic.labels[0]
+      post_label = post.labels[0]
+
+      expect(topic_label).to eql(post_label)
+    end
+  end
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,7 +4,9 @@ require 'random_data'
 RSpec.describe Topic, :type => :model do
   let(:topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
 
-  it {is_expected.to have_many(:posts)}
+  it { is_expected.to have_many(:posts) }
+  it { is_expected.to have_many(:labelings) }
+  it { is_expected.to have_many(:labels).through(:labelings) }
 
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:description) }
