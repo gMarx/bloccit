6e90223e4c96001b7f187861cd23b8a7c305fc9f
Gary Marx <gary@zenpayroll.com>
2016-03-10 18:10:06 -0800

    create favorites and implement in show

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   before_save { self.email = email.downcase }
   before_save :format_name
@@ -19,6 +20,10 @@ class User < ActiveRecord::Base
 
   def format_name
     self.name = name.split.map { |name| name.capitalize }.join(' ') if name
+  end
+
+  def favorite_for(post)
+    favorites.where(post_id: post.id).first
   end
 
 end
