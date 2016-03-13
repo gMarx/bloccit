17062c93eece0499453aeac63b0a846057e20a8a
Gary Marx <gary@zenpayroll.com>
2016-03-13 13:05:23 -0700

    refactor

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -34,28 +34,28 @@ RSpec.describe Post, :type => :model do
     end
   end
 
-  describe "voting" do
+  describe 'voting' do
     before do
-      3.times { post.votes.create!(value: 1) }
+      3.times { create(value: 1) }
       2.times { post.votes.create!(value: -1) }
       @up_votes = post.votes.where(value: 1).count
       @down_votes = post.votes.where(value: -1).count
     end
 
-   describe "#up_votes" do
-       it "counts the number of votes with value = 1" do
+   describe '#up_votes' do
+       it 'counts the number of votes with value = 1' do
         expect( post.up_votes ).to eq(@up_votes)
       end
    end
 
-    describe "#down_votes" do
-      it "counts the number of votes with value = -1" do
+    describe '#down_votes' do
+      it 'counts the number of votes with value = -1' do
         expect( post.down_votes ).to eq(@down_votes)
       end
     end
 
-    describe "#points" do
-      it "returns the sum of all down and up votes" do
+    describe '#points' do
+      it 'returns the sum of all down and up votes' do
         expect( post.points ).to eq(@up_votes - @down_votes)
       end
     end
@@ -66,13 +66,13 @@ RSpec.describe Post, :type => :model do
         expect(post.rank).to eq (post.points + (post.created_at - Time.new(1970,1,1)) / 1.day.seconds)
       end
 
-      it "updates the rank when an up vote is created" do
+      it 'updates the rank when an up vote is created' do
         old_rank = post.rank
         post.votes.create!(value: 1)
         expect(post.rank).to eq (old_rank + 1)
       end
 
-      it "updates the rank when a down vote is created" do
+      it 'updates the rank when a down vote is created' do
         old_rank = post.rank
         post.votes.create!(value: -1)
         expect(post.rank).to eq (old_rank - 1)
