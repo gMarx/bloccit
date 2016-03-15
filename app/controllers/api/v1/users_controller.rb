12e84cf02a55aef07ce06204ac3796fc20df83e7
Gary <gamarx@gmail.com>
2016-03-14 20:38:25 -0700

    update users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,4 +11,25 @@ class Api::V1::UsersController < Api::V1::BaseController
     users = User.all
     render json: users.to_json, status: 200
   end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update_attributes(user_params)
+      render json: user.to_json, status: 200
+    else
+      render json: {error: 'User update failed', status: 400}, status: 400
+    end
+  end
+
+  def create
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :role)
+  end
 end
+
+# curl -H "Content-type: application/json" -H "Authorization: Token /HfHktDdsLK+ro8mEWKbXnOpLMM+E6jkkmOyOB9PoVzNW4zIt/y9hkY1oEmANrUAn8LqdFFpN+fDxyK6wax+eA=="" -X PUT -d '{"user": {"name":"Billy Bob", "password":"helloworld"}}' http://localhost:3000/api/v1/users/1/
